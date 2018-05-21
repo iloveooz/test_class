f560e0031341a1dc78e1d46606b07dc8e39f5cf6
iloveooz <iloveooz@yandex.ru>
2018-05-21 17:16:02 +0700

    Добавил класс солдата, пронаследовались от класса фермера

--- game.cpp
+++ game.cpp
@@ -14,7 +14,23 @@ class Unit {
 		Unit(): health(100) { }
 		
 		Unit(int a): health(a) { }
-	};
+};
+	
+class Soldier : public Unit {
+	private:
+		int damage;
+	public:
+		void ShowDamage() {
+			cout << "Soldier damage: " << damage << '\n';
+		}
+		
+		Soldier():damage(16) { }
+		
+		Soldier(int a):damage(a) { }
+		
+		Soldier(int a, int b): Unit(a), damage(b) { }
+};
+	
 	
 	int main () {
 		Unit Kyle;
@@ -22,6 +38,14 @@ class Unit {
 		
 		Unit Jack(80);
 		Jack.ShowHealth();
+		
+		Soldier Hulk(90);
+		Hulk.ShowHealth();
+		Hulk.ShowDamage();
+		
+		Soldier Achile(16, 60);
+		Achile.ShowHealth();
+		Achile.ShowDamage();
 		
 		return 0;
 	}
